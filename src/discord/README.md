bf618cc134b0a4ed8f43d2489390897ecee7ffaf
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2023-07-25 08:38:07 +0200

    feat(discord): initialize project (#3)

---
+++ src/discord/README.md
@@ -0,0 +1,67 @@
+<p align="center"><h1 align="center">
+  Discord notifier
+</h1></p>
+
+<p align="center">
+  Send Sigyn alerts to Discord via webhook
+</p>
+
+<p align="center">
+  <a href="https://github.com/MyUnisoft/sigyn/src/discord">
+    <img src="https://img.shields.io/github/package-json/v/sigyn/discord?style=for-the-badge" alt="npm version">
+  </a>
+   <a href="https://github.com/MyUnisoft/sigyn/tree/main/src/discord">
+    <img src="https://img.shields.io/npm/dw/@sigyn/discord?style=for-the-badge" alt="download">
+  </a>
+  <a href="https://github.com/MyUnisoft/sigyn/src/discord">
+    <img src="https://img.shields.io/bundlephobia/min/@sigyn/discord?style=for-the-badge" alt="size">
+  </a>
+    <img src="https://api.securityscorecards.dev/projects/github.com/MyUnisoft/sigyn/badge?style=for-the-badge" alt="ossf scorecard">
+  </a>
+  <a href="https://github.com/MyUnisoft/sigyn/tree/main/src/discord">
+    <img src="https://img.shields.io/github/actions/workflow/status/MyUnisoft/sigyn/discord.yml?style=for-the-badge">
+  </a>
+  <a href="https://github.com/MyUnisoft/sigyn/tree/main/src/LICENSE">
+    <img src="https://img.shields.io/github/license/MyUnisoft/sigyn?style=for-the-badge" alt="license">
+  </a>
+</p>
+
+## 🚧 Requirements
+
+- [Node.js](https://nodejs.org/en/) version 18 or higher
+
+## 🚀 Getting Started
+
+This package is available in the Node Package Repository and can be easily installed with [npm](https://doc.npmjs.com/getting-started/what-is-npm) or [yarn](https://yarnpkg.com)
+
+```bash
+$ npm i @sigyn/discord
+# or
+$ yarn add @sigyn/discord
+```
+
+## 📚 Usage
+
+Add the Discord notifier to your Sigyn config:
+
+```json
+
+{
+  "notifiers": {
+    "@sigyn/discord": {
+      "webhookUrl": "https://discord.com/api/webhooks/xxx/yyy"
+    },
+    ...
+  },
+  "rules": [
+    ...
+  ]
+}
+```
+
+**Webhook URL**
+
+Using Discord webhooks is very simple: go to you server settings, **APPS** > **Integrations** > **Webhooks** then choose or create your webhook and click **Copy Webhook URL** button 🎉
+
+## License
+MIT
---
+++ src/discord/test/executeWebhook.spec.ts
@@ -0,0 +1,111 @@
+// Import Node.js Dependencies
+import assert from "node:assert";
+import { after, before, describe, it } from "node:test";
+
+// Import Third-party Dependencies
+import { MockAgent, MockPool, getGlobalDispatcher, setGlobalDispatcher } from "@myunisoft/httpie";
+
+// Import Internal Dependencies
+import * as discord from "../src/index";
+
+const kMockAgent = new MockAgent();
+const kDispatcher = getGlobalDispatcher();
+const kValidRuleConfig = {
+  name: "test1",
+  logql: "{app=\"foo\", env=\"prod\"} |= `One of the file names does not match what is expected`",
+  polling: "1m",
+  alert: {
+    on: {
+      count: 6,
+      interval: "5m"
+    },
+    template: {
+      title: "🚨 {ruleName} - Triggered {counter} times!",
+      content: [
+        "- LogQL: `{logql}`",
+        "- Threshold: **{count}**",
+        "- Interval: **{interval}**",
+        "- Polling: **{polling}**"
+      ]
+    }
+  }
+};
+const kDummyWebhoobURL = "https://foo.com";
+
+describe("executeWebhook()", () => {
+  let pool: MockPool;
+
+  before(() => {
+    setGlobalDispatcher(kMockAgent);
+    pool = kMockAgent.get(kDummyWebhoobURL);
+    kMockAgent.disableNetConnect();
+  });
+
+  after(() => {
+    kMockAgent.enableNetConnect();
+    setGlobalDispatcher(kDispatcher);
+  });
+
+  it("should throws if there is no title AND no content", async() => {
+    await assert.rejects(async() => {
+      await discord.execute({
+        counter: 10,
+        ruleConfig: { ...kValidRuleConfig, alert: { ...kValidRuleConfig.alert, template: {} } },
+        webhookUrl: kDummyWebhoobURL
+      });
+    }, {
+      name: "Error",
+      message: "Invalid rule template: one of the title or content is required."
+    });
+  });
+
+  it("should execute webhook", async() => {
+    pool.intercept({
+      method: "POST",
+      path: "/"
+    }).reply(200, { foo: "bar" });
+
+    const { data } = await discord.execute({
+      counter: 10,
+      ruleConfig: kValidRuleConfig,
+      webhookUrl: kDummyWebhoobURL
+    });
+
+    assert.deepEqual(JSON.parse(data), { foo: "bar" });
+  });
+
+  it("should fail executing webhook when discord API rejects", async() => {
+    pool.intercept({
+      method: "POST",
+      path: "/"
+    }).reply(400, { message: "Unable to send webhook" });
+
+    await assert.rejects(async() => await discord.execute({
+      counter: 10,
+      ruleConfig: kValidRuleConfig,
+      webhookUrl: kDummyWebhoobURL
+    }), {
+      name: "Error",
+      message: "Bad Request"
+    });
+  });
+
+  it("should fail executing webhook when given missing config", async() => {
+    pool.intercept({
+      method: "POST",
+      path: "/"
+    }).reply(200, { foo: "bar" });
+
+    await assert.rejects(async() => await discord.execute({
+      counter: 10,
+      ruleConfig: {
+        ...kValidRuleConfig,
+        alert: { ...kValidRuleConfig.alert, on: { ...kValidRuleConfig.alert.on, count: undefined } }
+      } as any,
+      webhookUrl: kDummyWebhoobURL
+    }), {
+      name: "MissingValueError",
+      message: "Missing a value for the placeholder: count"
+    });
+  });
+});
