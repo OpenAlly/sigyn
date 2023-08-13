0a285a8965fd3957fd499312365109d563467f7d
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2023-08-13 14:24:45 +0200

    test(agent): add Rule tests (#53)
    * test(agent): add Rule tests
    
    * mock Loki API (config needs to fetch labels)
    
    * ci: max delay for handleLogs
    
    * perf: improve walkOnLogs
    
    * chore: make coverage accurate
    
    * fix: null accumulatedCounters

--- src/agent/src/rules.ts
+++ src/agent/src/rules.ts
@@ -1,17 +1,15 @@
 /* eslint-disable max-len */
 // Import Third-party Dependencies
-import { SigynRule, getConfig } from "@sigyn/config";
-import { GrafanaLoki } from "@myunisoft/loki";
+import { SigynRule } from "@sigyn/config";
 import dayjs from "dayjs";
 import ms from "ms";
 import cronParser from "cron-parser";
+import { Database } from "better-sqlite3";
 
 // Import Internal Dependencies
-import { DbCounter, DbRule, getDB } from "./database";
+import { DbRule, getDB } from "./database";
 import * as utils from "./utils";
-import { createAlert } from "./alert";
 import { Logger } from ".";
-import { Database } from "better-sqlite3";
 
 // CONSTANTS
 export const DEFAULT_POLLING = "1m";
@@ -31,13 +29,12 @@ export class Rule {
     this.#config = rule;
   }
 
-
-  #getRuleFromDatabase(): DbRule {
+  getRuleFromDatabase(): DbRule {
     return getDB().prepare("SELECT * FROM rules WHERE name = ?").get(this.#config.name) as DbRule;
   }
 
   init(): void {
-    const databaseRule = this.#getRuleFromDatabase();
+    const databaseRule = this.getRuleFromDatabase();
 
     if (databaseRule === undefined) {
       getDB().prepare("INSERT INTO rules (name) VALUES (?)").run(this.#config.name);
@@ -46,92 +43,85 @@ export class Rule {
     }
   }
 
-  async handleLogs(): Promise<void> {
+  async walkOnLogs(logs: string[]): Promise<boolean> {
     const db = getDB();
 
-    const lokiApi = new GrafanaLoki({
-      remoteApiURL: getConfig().loki.apiUrl
-    });
-    const logs = await lokiApi.queryRange(this.#config.logql, {
-      start: this.#getQueryRangeStartUnixTimestamp()
-    });
-
-    const rule = this.#getRuleFromDatabase();
-    const now = dayjs().unix();
+    const rule = this.getRuleFromDatabase();
+    const now = dayjs().valueOf();
     const timeThreshold = utils
       .durationOrCronToDate(this.#config.alert.on.interval, "subtract")
-      .unix();
+      .valueOf();
 
-    const countersInThresholdInterval = db.prepare("SELECT * FROM counters WHERE ruleId = ? AND timestamp >= ?").all(
-      rule.id,
-      timeThreshold
-    ) as DbCounter[];
-    const accumulatedCounters = countersInThresholdInterval.reduce((acc, cur) => acc + cur.counter, 0);
+    const accumulatedCounters = (db
+      .prepare("SELECT SUM(counter) as totalCounter FROM counters WHERE ruleId = ? AND timestamp >= ?")
+      .get(rule.id, timeThreshold) as { totalCounter: number }).totalCounter ?? 0;
 
-    rule.counter -= rule.counter - accumulatedCounters;
-    rule.counter += logs.length;
-    db.prepare("UPDATE rules SET counter = ? WHERE id = ?").run(
-      rule.counter,
-      rule.id
-    );
-    db.prepare("INSERT INTO counters (ruleId, counter, timestamp) VALUES (?, ?, ?)").run(
-      rule.id,
-      logs.length,
-      now
-    );
-    db.prepare("DELETE FROM counters WHERE ruleId = ? AND timestamp < ?").run(
-      rule.id,
-      timeThreshold
-    );
+    // rule.counter has may not been updated yet, so we need to substract the diff with accumulatedCounters
+    // i.e rule.counter = 10, but we have 5 counters in the DB within the interval, so we need to substract 5 to the rule.counter
+    rule.counter = accumulatedCounters + logs.length;
+
+    db.transaction(() => {
+      db.prepare("UPDATE rules SET counter = ? WHERE id = ?").run(rule.counter, rule.id);
+      db.prepare("INSERT INTO counters (ruleId, counter, timestamp) VALUES (?, ?, ?)").run(rule.id, logs.length, now);
+      db.prepare("DELETE FROM counters WHERE ruleId = ? AND timestamp < ?").run(rule.id, timeThreshold);
+    })();
 
     const alertThreshold = this.#config.alert.on.count;
     this.#logger.info(`[${rule.name}](state: handle|previous: ${accumulatedCounters}|new: ${logs.length}|next: ${rule.counter}|threshold: ${alertThreshold})`);
 
     const [operator, value] = utils.ruleCountThresholdOperator(alertThreshold);
-    if (!utils.ruleCountMatchOperator(operator, rule.counter, value)) {
-      return;
-    }
 
     if (operator.startsWith("<")) {
       // we checking for a max value, so we want to wait the whole interval before sending an alert
-      const counters = db.prepare("SELECT * FROM counters WHERE ruleId = ?").all(
-        rule.id
-      ) as DbCounter[];
+      const countersAndSum = db.prepare(`
+        SELECT COUNT(id) as count, SUM(counter) as totalCounter
+        FROM counters
+        WHERE ruleId = ?
+      `).get(rule.id) as { count: number, totalCounter: number };
 
-      const diffPolling = dayjs().unix() - utils.durationOrCronToDate(this.#getCurrentPolling()[1], "subtract").unix();
-      const diffInterval = dayjs().unix() - timeThreshold;
-      const expectedCounterCount = Math.ceil(diffInterval / diffPolling);
+      const counters = countersAndSum.count;
+      const accumulatedCountersInInterval = countersAndSum.totalCounter;
 
-      // check it fetch since the rule interval (i.e if interval is 1m and we fetch every 30s, we want to check there is at least 2 counters)
-      if (counters.length < expectedCounterCount) {
-        return;
+      const diffPolling = now - utils.durationOrCronToDate(this.#getCurrentPolling()[1], "subtract").valueOf();
+      const diffInterval = now - timeThreshold;
+      const expectedCounterCount = Math.floor(diffInterval / diffPolling);
+
+      if (counters < expectedCounterCount) {
+        return false;
       }
 
-      const countInInterval = counters.reduce((acc, cur) => acc + cur.counter, 0);
-      if (!utils.ruleCountMatchOperator(operator, countInInterval, value)) {
-        return;
+      if (!utils.ruleCountMatchOperator(operator, accumulatedCountersInInterval, value)) {
+        return false;
       }
+    }
+    else if (!utils.ruleCountMatchOperator(operator, rule.counter, value)) {
+      return false;
     }
 
     const cancelAlert = this.#checkThrottle(rule, db);
     if (cancelAlert) {
-      return;
-    }
-
-    this.#logger.error(`[${rule.name}](state: alert|threshold: ${alertThreshold}|actual: ${rule.counter})`);
-    createAlert(rule, this.#config, this.#logger);
-
-    db.prepare("UPDATE rules SET counter = 0 WHERE id = ?").run(rule.id);
-    db.prepare("DELETE from counters WHERE ruleId = ?").run(rule.id);
-  }
-
-  #checkThrottle(rule: DbRule, db: Database): boolean {
-    if (!this.#config.alert.throttle) {
       return false;
     }
 
-    const { interval, count = 0 } = this.#config.alert.throttle;
-    const intervalDate = utils.durationOrCronToDate(interval, "subtract").unix();
+    this.#logger.error(`[${rule.name}](state: alert|threshold: ${alertThreshold}|actual: ${rule.counter})`);
+
+    db.transaction(() => {
+      db.prepare("UPDATE rules SET counter = 0 WHERE id = ?").run(rule.id);
+      db.prepare("DELETE from counters WHERE ruleId = ?").run(rule.id);
+    })();
+
+    return true;
+  }
+
+  #checkThrottle(rule: DbRule, db: Database): boolean {
+    const { throttle } = this.#config.alert;
+
+    if (!throttle) {
+      return false;
+    }
+
+    const { interval, count = 0 } = throttle;
+    const intervalDate = utils.durationOrCronToDate(interval, "subtract").valueOf();
     const ruleAlertsCount = db.prepare("SELECT * FROM alerts WHERE ruleId = ? AND createdAt >= ?").all(
       rule.id,
       intervalDate
@@ -144,30 +134,26 @@ export class Rule {
     }
 
     if (ruleAlertsCount > 0 && (ruleAlertsCount + rule.throttleCount) < count) {
-      db.prepare("UPDATE rules SET throttleCount = ? WHERE id = ?").run(
-        rule.throttleCount + 1,
-        rule.id
-      );
-      db.prepare("UPDATE rules SET counter = 0 WHERE id = ?").run(rule.id);
-      db.prepare("DELETE from counters WHERE ruleId = ? AND timestamp <= ?").run(rule.id, intervalDate);
+      db.transaction(() => {
+        db.prepare("UPDATE rules SET throttleCount = ?, counter = 0 WHERE id = ?").run(rule.throttleCount + 1, rule.id);
+        db.prepare("DELETE from counters WHERE ruleId = ? AND timestamp <= ?").run(rule.id, intervalDate);
+      })();
 
       this.#logger.error(`[${rule.name}](state: throttle|count: ${count}|actual: ${ruleAlertsCount}|throttle: ${rule.throttleCount})`);
 
       return true;
     }
 
-    db.prepare("UPDATE rules SET throttleCount = 0 WHERE id = ?").run(
-      rule.id
-    );
+    db.prepare("UPDATE rules SET throttleCount = 0 WHERE id = ?").run(rule.id);
 
     return false;
   }
 
-  #getQueryRangeStartUnixTimestamp(): number {
-    const rule = this.#getRuleFromDatabase();
+  getQueryRangeStartUnixTimestamp(): number {
+    const rule = this.getRuleFromDatabase();
     const now = dayjs();
 
-    getDB().prepare("UPDATE rules SET lastRunAt = ? WHERE id = ?").run(now.unix(), rule.id);
+    getDB().prepare("UPDATE rules SET lastRunAt = ? WHERE id = ?").run(now.valueOf(), rule.id);
 
     const [isCron, polling] = this.#getCurrentPolling();
 
@@ -183,7 +169,8 @@ export class Rule {
       const diff = Math.abs(dayjs.unix(rule.lastRunAt!).diff(now, "ms"));
 
       if (diff <= Number(ms(polling))) {
-        return rule.lastRunAt;
+        // we store ms unix in DB while Loki API takes seconds unix
+        return rule.lastRunAt / 1000;
       }
     }
 
