3a8df78d3fefa900d6b734acf910e80be7ae83db
PierreDemailly <pierredemailly.pro@gmail.com>
2023-09-29 10:34:45 +0200

    refactor(agent): move asyncTask agent failure handling into a dedicated function

--- src/agent/src/tasks/asyncTask.ts
+++ src/agent/src/tasks/asyncTask.ts
@@ -1,14 +1,13 @@
 // Import Third-party Dependencies
-import { getConfig, SigynInitializedRule } from "@sigyn/config";
+import { SigynInitializedRule } from "@sigyn/config";
 import { GrafanaLoki } from "@myunisoft/loki";
 import { AsyncTask } from "toad-scheduler";
 
 // Import Internal Dependencies
 import { Rule } from "../rules";
 import { Logger } from "..";
-import { createAgentFailureAlert, createRuleAlert } from "../alert";
-import { DbAgentFailure, getDB } from "../database";
-import { checkAgentFailureThrottle } from "../utils/selfMonitoring";
+import { createRuleAlert } from "../alert";
+import { handleAgentFailure } from "../utils/selfMonitoring";
 
 export interface AsyncTaskOptions {
   logger: Logger;
@@ -40,41 +39,7 @@ export function asyncTask(ruleConfig: SigynInitializedRule, options: AsyncTaskOp
     catch (e) {
       logger.error(`[${ruleConfig.name}](error: ${e.message})`);
 
-      const config = getConfig();
-      if (!config.selfMonitoring) {
-        return;
-      }
-      const { errorFilters, ruleFilters, minimumErrorCount = 0 } = config.selfMonitoring;
-
-      if (errorFilters && !errorFilters?.includes(e.message)) {
-        return;
-      }
-
-      if (ruleFilters && !ruleFilters?.includes(ruleConfig.name)) {
-        return;
-      }
-
-      try {
-        const dbRule = rule.getRuleFromDatabase();
-        getDB().prepare("INSERT INTO agentFailures (ruleId, message, timestamp) VALUES (?, ?, ?)").run(
-          dbRule.id,
-          e.message,
-          Date.now()
-        );
-
-        const agentFailures = getDB().prepare("SELECT * FROM agentFailures").all() as DbAgentFailure[];
-        if (agentFailures.length > minimumErrorCount) {
-          if (checkAgentFailureThrottle(config.selfMonitoring.throttle)) {
-            return;
-          }
-
-          createAgentFailureAlert(agentFailures, config.selfMonitoring, logger);
-          getDB().prepare("DELETE FROM agentFailures").run();
-        }
-      }
-      catch (err) {
-        logger.error(`[SELF MONITORING](error: ${err.message})`);
-      }
+      handleAgentFailure(e.message, rule, logger);
     }
   });
 
--- src/agent/src/utils/selfMonitoring.ts
+++ src/agent/src/utils/selfMonitoring.ts
@@ -1,12 +1,22 @@
 // Import Third-party Dependencies
-import { SigynSelfMonitoring } from "@sigyn/config";
+import { SigynSelfMonitoring, getConfig } from "@sigyn/config";
 
 // Import Internal Dependencies
-import { getDB } from "../database";
+import { DbAgentFailure, getDB } from "../database";
 import { AgentFailureAlert } from "../notifier";
 import * as cronUtils from "./cron";
+import { Rule } from "../rules";
+import { Logger } from "..";
+import { createAgentFailureAlert } from "../alert";
 
-export function checkAgentFailureThrottle(throttle: SigynSelfMonitoring["throttle"]) {
+export function getAgentFailureRules(alert: AgentFailureAlert): string {
+  const ruleIds = new Set(alert.failures.map(({ ruleId }) => ruleId));
+  const failures = getDB().prepare("SELECT name FROM rules WHERE id IN (?)").all([...ruleIds]) as { name: string }[];
+
+  return failures.map(({ name }) => name).join(", ");
+}
+
+function hasAgentFailureThrottle(throttle: SigynSelfMonitoring["throttle"]) {
   if (!throttle) {
     return false;
   }
@@ -21,9 +31,40 @@ export function checkAgentFailureThrottle(throttle: SigynSelfMonitoring["throttl
   return agentFailuresCount <= count;
 }
 
-export function getAgentFailureRules(alert: AgentFailureAlert): string {
-  const ruleIds = new Set(alert.failures.map(({ ruleId }) => ruleId));
-  const failures = getDB().prepare("SELECT name FROM rules WHERE id IN (?)").all([...ruleIds]) as { name: string }[];
+export function handleAgentFailure(errorMessage: string, rule: Rule, logger: Logger) {
+  const config = getConfig();
+  if (!config.selfMonitoring) {
+    return;
+  }
+  const { errorFilters, ruleFilters, minimumErrorCount = 0 } = config.selfMonitoring;
 
-  return failures.map(({ name }) => name).join(", ");
+  if (errorFilters && !errorFilters?.includes(errorMessage)) {
+    return;
+  }
+
+  if (ruleFilters && !ruleFilters?.includes(rule.config.name)) {
+    return;
+  }
+
+  try {
+    const dbRule = rule.getRuleFromDatabase();
+    getDB().prepare("INSERT INTO agentFailures (ruleId, message, timestamp) VALUES (?, ?, ?)").run(
+      dbRule.id,
+      errorMessage,
+      Date.now()
+    );
+
+    const agentFailures = getDB().prepare("SELECT * FROM agentFailures").all() as DbAgentFailure[];
+    if (agentFailures.length > minimumErrorCount) {
+      if (hasAgentFailureThrottle(config.selfMonitoring.throttle)) {
+        return;
+      }
+
+      createAgentFailureAlert(agentFailures, config.selfMonitoring, logger);
+      getDB().prepare("DELETE FROM agentFailures").run();
+    }
+  }
+  catch (error) {
+    logger.error(`[SELF MONITORING](error: ${error.message})`);
+  }
 }
