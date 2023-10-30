20f866954c0a31f1cb175086fc84f77a880765bd
PierreDemailly <pierredemailly.pro@gmail.com>
2023-10-30 10:18:09 +0100

    chore(morphix): split functions into multiple files

--- src/morphix/src/functions/dnsresolve.ts
+++ src/morphix/src/functions/dnsresolve.ts
@@ -1,10 +1,6 @@
 // Import Node.js Dependencies
 import dns from "node:dns/promises";
 
-export function capitalize(value: string): string {
-  return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
-}
-
 export async function dnsresolve(ip: string): Promise<string> {
   try {
     const hostNames = await dns.reverse(ip);
