c0122ad834b77409c61f57e39aa8f2376080cc36
fraxken <gentilhomme.thomas@gmail.com>
2023-11-23 21:43:41 +0100

    refactor(types): clean naming & improve syntax

--- src/pattern/src/types.ts
+++ src/pattern/src/types.ts
@@ -1,30 +1,35 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-type Split<S extends string, D extends string> =
-    string extends S ? Array<string> :
-    S extends "" ? [] :
-    S extends `${infer T}${D}${infer U}` ? [`${T}${D}`, ...Split<U, D>] : [S];
-
-type Trim<T> = T extends `${" "}${infer U}` ?
-  Trim<U> : T extends `${infer U}${" "}` ? Trim<U> : T;
-
-type ExtractPattern<Pattern extends string> = Pattern extends `${infer _}<${infer Name}>${infer _}` ?
-  Name extends "_" ? never : Trim<Name> : never;
+type Simplify<T> = {[KeyType in keyof T]: T[KeyType]} & {};
 
 type TupleToObject<T extends Array<string>> = {
   [key in ExtractPattern<T[number]>]: string
 };
-type Simplify<T> = {[KeyType in keyof T]: T[KeyType]} & {};
+
+type Split<S extends string, D extends string> =
+  string extends S ? Array<string> :
+  S extends "" ? [] :
+  S extends `${infer T}${D}${infer U}` ? [`${T}${D}`, ...Split<U, D>] : [S];
+
+type Trim<T> = T extends `${" "}${infer U}` ?
+  Trim<U> : T extends `${infer U}${" "}` ? Trim<U> : T;
 
 type Concat<T extends Array<string>> = T extends [
   infer F extends string,
   ...infer R extends Array<string>
 ] ? `${F} ${Concat<R>}` : "";
 
-type InlineStr<T extends LokiPatternType> =
+type ArrayToString<T extends LokiPatternType> =
   T extends Array<string> ? Concat<T> :
   T extends ReadonlyArray<string> ? Concat<[...T]> : T;
 
+type ExtractPattern<Pattern extends string> = Pattern extends `${infer _}<${infer Name}>${infer _}` ?
+  Name extends "_" ? never : Trim<Name> : never;
+
 export type LokiPatternType = string | Array<string> | ReadonlyArray<string>;
-export type LokiLiteralPattern<T extends LokiPatternType> = Simplify<TupleToObject<Split<InlineStr<T>, ">">>>;
+export type LokiLiteralPattern<T extends LokiPatternType> = Simplify<
+  TupleToObject<
+    Split<ArrayToString<T>, ">">
+  >
+>;
